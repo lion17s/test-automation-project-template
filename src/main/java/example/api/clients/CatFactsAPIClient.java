583da643514a5aa82d63c63dcfa70421c18232c2
lion17s <serhii.sheptytskyi@gmail.com>
2021-03-22 21:55:05 +0200

    Add annotation

--- src/main/java/example/api/clients/CatFactsAPIClient.java
+++ src/main/java/example/api/clients/CatFactsAPIClient.java
@@ -1,12 +1,14 @@
 package example.api.clients;
 
 import com.ta.core.api.BaseAPIClient;
+import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 
 public class CatFactsAPIClient extends BaseAPIClient<CatFactsAPIClient> {
 
     private static final String BASE_PATH = "/facts";
 
+    @Step
     public CatFactsAPIClient getRandomFact() {
         setResponse(RestAssured.given().basePath(BASE_PATH).get("/random"));
         return this;
