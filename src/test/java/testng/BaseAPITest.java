8a236725d86aa4326ce26261d9430e93069deab8
lion17s <serhii.sheptytskyi@gmail.com>
2021-03-22 21:42:29 +0200

    Add api example configuration

--- src/test/java/testng/BaseAPITest.java
+++ src/test/java/testng/BaseAPITest.java
@@ -1,9 +1,17 @@
 package testng;
 
+import com.ta.core.env.Environment;
 import com.ta.core.testng.listeners.APITestListener;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 
 @Listeners(APITestListener.class)
 public abstract class BaseAPITest extends BaseTest {
+
+    @BeforeSuite(groups = "api.example.test")
+    public void setupAPIClientEnvironment() {
+        RestAssured.baseURI = Environment.get().getString("base.uri");
+    }
 
 }
